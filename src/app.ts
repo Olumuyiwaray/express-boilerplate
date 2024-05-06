9a2658e5d0003ae499c45c0c235b1aa16a0c2b38
Samuel Jimoh <95105034+Olumuyiwaray@users.noreply.github.com>
2024-05-06 16:53:39 +0100

    Add more input sanitization packages (#1)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-import express, { Application, NextFunction, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 import routes from './routes/routes';
 import methodOverride from 'method-override';
+import hpp from 'hpp';
+import mongoSanitze from 'express-mongo-sanitize';
 import loggerMiddleware from './middlewares/loggerMiddleware';
 import errorHandlingMiddleware from './middlewares/errorHandler';
 import { enviromentConfig, limiter } from './config/config';
-import morgan from 'morgan';
 
 const app: Application = express();
 
@@ -22,8 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
-app.use(compression());
 app.use(methodOverride('_method'));
+app.use(hpp());
+app.use(mongoSanitze());
+app.use(compression());
 
 // -------- import logging middleware -------- //
 app.use(loggerMiddleware);
@@ -32,7 +36,7 @@ app.use(loggerMiddleware);
 app.use(routes);
 
 // -------- handle 404 errors --------- //
-app.use((req: Request, res: Response, next: NextFunction) => {
+app.use((req: Request, res: Response) => {
   const err = new Error(`path ${req.originalUrl} not found`);
   res.status(404).json({ message: err.message });
 });
